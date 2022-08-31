cd9ce4446033e797d8a738c4a2681371376d147e
makeevs74 <makeevs74@gmail.com>
2022-08-31 22:43:40 +0500

    Initial Commit

---
+++ HW_13_SettingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_13_SettingView
+//
+//  Created by Sergey Makeev on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
