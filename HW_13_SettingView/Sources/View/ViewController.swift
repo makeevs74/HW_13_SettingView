984be02013ad81824050f04c41a610702f50b1c4
makeevs74 <makeevs74@gmail.com>
2022-09-01 00:23:45 +0500

    add custom cell to ViewController
    make setupHierarchy and setupLayout. And add extension

--- HW_13_SettingView/Sources/View/ViewController.swift
+++ HW_13_SettingView/Sources/View/ViewController.swift
@@ -6,13 +6,64 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets -
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.register(CustomCellUser.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+
+
+
+    // MARK: - Lifecycle -
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemGray6
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupHierarchy()
+        setupLayout()
     }
 
+    // MARK: - Setup -
 
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.right.bottom.left.equalTo(view)
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CustomCellUser
+        cell?.accessoryType = .disclosureIndicator
+        return cell ?? UITableViewCell()
+    }
 }
 
