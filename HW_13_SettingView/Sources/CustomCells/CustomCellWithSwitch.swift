dd248397f6d38fcc0b68e4c06d4663f3affd7f99
makeevs74 <makeevs74@gmail.com>
2022-09-08 15:16:54 +0500

    Correct Model , CustomCells

---
+++ HW_13_SettingView/Sources/CustomCells/CustomCellWithSwitch.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCellWithSwitch.swift
+//  HW_13_SettingView
+//
+//  Created by Sergey Makeev on 07.09.2022.
+//
+
+import UIKit
+
+class CustomCellWithSwitch: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
